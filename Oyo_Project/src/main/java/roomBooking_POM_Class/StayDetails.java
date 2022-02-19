f7e13ef2bf59740b2b926fae91fa4b3fbd9d9148
ADMIN <ADMIN@DESKTOP-CQUQF6I>
2022-02-19 11:31:11 +0530

    Excel Sheet

--- Oyo_Project/src/main/java/roomBooking_POM_Class/StayDetails.java
+++ Oyo_Project/src/main/java/roomBooking_POM_Class/StayDetails.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class StayDetails 
+public class StayDetails  
 {
 	// after defined Global Vairables ///////////////////////////////////////////
 	private WebDriver driver;
